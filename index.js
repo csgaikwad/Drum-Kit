98ec01dc0729dadc0b91e1bddc40ebf569ddd0c7
Shekhar <131723143+csgaikwad@users.noreply.github.com>
2024-01-23 04:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 $(".drum").each(function () {
   $(this).click(function () {
     let key = this.innerHTML;
@@ -27,27 +28,35 @@ function play(keyORpress) {
     case "w":
       a = new Audio("sounds/crash.mp3");
       break;
+      
     case "a":
       a = new Audio("sounds/kick.mp3");
       break;
+      
     case "s":
       a = new Audio("sounds/snare.mp3");
       break;
+      
     case "d":
       a = new Audio("sounds/tom-1.mp3");
       break;
+      
     case "j":
       a = new Audio("sounds/tom-2.mp3");
       break;
+      
     case "k":
       a = new Audio("sounds/tom-3.mp3");
       break;
+      
     case "l":
       a = new Audio("sounds/tom-4.mp3");
       break;
+      
     default:
       console.log("Error at switch");
       return; 
+      
   }
 
   a.play();
